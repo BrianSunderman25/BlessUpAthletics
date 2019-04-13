76812b97a6e937c1b3397ca3be4b0d2533331e21
BrianSunderman25 <brian.sunderman.25@outlook.com>
2019-04-13 15:06:04 -0400

    new template for home page, needs customized for bless up

--- BlessUpAthletics/BlessUpAthletics/ClientApp/src/app/merch/merch.component.ts
+++ BlessUpAthletics/BlessUpAthletics/ClientApp/src/app/merch/merch.component.ts
@@ -1,12 +1,21 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-about',
   templateUrl: './merch.component.html'
 })
 export class MerchComponent {
+  public queryStuff() {
+    
+  }
+
+  
+
 
 
 
 }
+
+
