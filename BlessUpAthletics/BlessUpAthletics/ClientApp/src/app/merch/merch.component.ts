9ea25d84fee2bfa71a9e9bf027733927581bff37
BrianSunderman25 <brian.sunderman.25@outlook.com>
2019-04-13 14:15:13 -0400

    trying a new theme, committing in case i dont like it

---
+++ BlessUpAthletics/BlessUpAthletics/ClientApp/src/app/merch/merch.component.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './merch.component.html'
+})
+export class MerchComponent {
+
+
+
+}
