e7e785fdaca38b00aa99ef7ecc3ef7b0fe690812
BrianSunderman25 <brian.sunderman.25@outlook.com>
2019-04-15 22:26:14 -0400

    need to add contact possibilities

---
+++ BlessUpAthletics/BlessUpAthletics/ClientApp/src/app/customers/customers.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './customers.component.html'
+})
+export class CustomersComponent {
+  
+
+
+
+
+
+
+}
+
+
